3f18cb1f1c4839e21289eb3a4739956c2b10cf27
Pedro Vitor <ped.frsouza@gmail.com>
2024-12-14 00:46:12 -0300

    refatoracao: adicao springboot

---
+++ src/main/java/com/maisprati/forum/Application.java
@@ -0,0 +1,13 @@
+package com.maisprati.forum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
