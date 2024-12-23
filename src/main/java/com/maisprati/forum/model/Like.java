8c71ee4c2f25fd77fdc73de276c164ccd98f2079
Pedro Vitor <ped.frsouza@gmail.com>
2024-12-22 21:06:30 -0300

    feat: Mapeamento das entidades modelos utilizando JPA

---
+++ src/main/java/com/maisprati/forum/model/Like.java
@@ -0,0 +1,57 @@
+package com.maisprati.forum.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "likes")
+public class Like {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne()
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne()
+    @JoinColumn(name = "topic_id")
+    private Topic topic;
+
+    @Column(updatable = false)
+    private LocalDateTime date = LocalDateTime.now();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Topic getTopic() {
+        return topic;
+    }
+
+    public void setTopic(Topic topic) {
+        this.topic = topic;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+}
