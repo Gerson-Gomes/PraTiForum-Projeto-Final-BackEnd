199517accb7defc9345ca221aed67e5cbffe8183
TEOFILO NICOLAU <teofilonicolau157@gmail.com>
2025-01-22 23:27:43 -0300

    Implementação do CRUD de Tags

--- src/main/java/com/maisprati/forum/service/TopicService.java
+++ src/main/java/com/maisprati/forum/service/TopicService.java
@@ -1,11 +1,13 @@
 package com.maisprati.forum.service;
 
 import com.maisprati.forum.dto.TopicDto;
-import com.maisprati.forum.model.Topic;
 import com.maisprati.forum.model.Response;
-import com.maisprati.forum.repository.TopicRepository;
+import com.maisprati.forum.model.Tag;
+import com.maisprati.forum.model.Topic;
 import com.maisprati.forum.repository.LikeRepository;
 import com.maisprati.forum.repository.ResponseRepository;
+import com.maisprati.forum.repository.TagRepository;
+import com.maisprati.forum.repository.TopicRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -20,6 +22,9 @@ public class TopicService {
     private TopicRepository topicRepository;
 
     @Autowired
+    private TagRepository tagRepository;
+
+    @Autowired
     private LikeRepository likeRepository;
 
     @Autowired
@@ -29,12 +34,21 @@ public class TopicService {
         Topic topic = new Topic();
         topic.setTitle(topicDto.getTitle());
         topic.setContent(topicDto.getContent());
+
+        // Associa as tags ao tópico
+        List<Tag> tags = topicDto.getTagIds().stream()
+                .map(tagRepository::findById)
+                .map(optionalTag -> optionalTag.orElseThrow(() -> new RuntimeException("Tag não encontrada")))
+                .collect(Collectors.toList());
+        topic.setTags(tags);
+
         Topic createdTopic = topicRepository.save(topic);
 
         TopicDto createdTopicDto = new TopicDto();
         createdTopicDto.setId(createdTopic.getId());
         createdTopicDto.setTitle(createdTopic.getTitle());
         createdTopicDto.setContent(createdTopic.getContent());
+        createdTopicDto.setTagIds(tags.stream().map(Tag::getId).collect(Collectors.toList()));
 
         return createdTopicDto;
     }
@@ -45,6 +59,7 @@ public class TopicService {
             topicDto.setId(topic.getId());
             topicDto.setTitle(topic.getTitle());
             topicDto.setContent(topic.getContent());
+            topicDto.setTagIds(topic.getTags().stream().map(Tag::getId).collect(Collectors.toList()));
             return topicDto;
         }).collect(Collectors.toList());
     }
@@ -60,12 +75,21 @@ public class TopicService {
         Topic existingTopic = topicRepository.findById(topicId).orElseThrow(() -> new RuntimeException("Tópico não encontrado"));
         existingTopic.setTitle(topicDto.getTitle());
         existingTopic.setContent(topicDto.getContent());
+
+        // Atualiza as tags associadas ao tópico
+        List<Tag> tags = topicDto.getTagIds().stream()
+                .map(tagRepository::findById)
+                .map(optionalTag -> optionalTag.orElseThrow(() -> new RuntimeException("Tag não encontrada")))
+                .collect(Collectors.toList());
+        existingTopic.setTags(tags);
+
         Topic updatedTopic = topicRepository.save(existingTopic);
 
         TopicDto updatedTopicDto = new TopicDto();
         updatedTopicDto.setId(updatedTopic.getId());
         updatedTopicDto.setTitle(updatedTopic.getTitle());
         updatedTopicDto.setContent(updatedTopic.getContent());
+        updatedTopicDto.setTagIds(tags.stream().map(Tag::getId).collect(Collectors.toList()));
 
         return updatedTopicDto;
     }
