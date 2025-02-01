19dc3834de64a9ea19a4ede409162b7c03942b32
TEOFILO NICOLAU <teofilonicolau157@gmail.com>
2025-02-01 01:21:56 -0300

    Resolvendo conflitos e recuperando melhorias na autenticação com Google

--- src/main/java/com/maisprati/forum/Application.java
+++ src/main/java/com/maisprati/forum/Application.java
@@ -1,13 +1,18 @@
 package com.maisprati.forum;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan; // Importação do ComponentScan
 
 @SpringBootApplication
+@ComponentScan("com.maisprati.forum") // A anotação ComponentScan precisa estar aqui
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
 }
+
