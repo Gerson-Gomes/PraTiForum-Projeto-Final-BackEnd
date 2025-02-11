65b5fbcdc8a41b0566a8b9386f6e4eb8b9d402e4
Pedro Vitor <ped.frsouza@gmail.com>
2025-02-11 10:23:08 -0300

    refactor: remove HTTP status response from exception classes

--- src/main/java/com/maisprati/forum/exception/TagAlreadyExistsException.java
+++ src/main/java/com/maisprati/forum/exception/TagAlreadyExistsException.java
@@ -1,9 +1,6 @@
 package com.maisprati.forum.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.CONFLICT)
 public class TagAlreadyExistsException extends RuntimeException {
     public TagAlreadyExistsException(String message) {
         super(message);
--- src/main/java/com/maisprati/forum/infra/RestExceptionHandler.java
+++ src/main/java/com/maisprati/forum/infra/RestExceptionHandler.java
@@ -1,7 +1,6 @@
 package com.maisprati.forum.infra;
 
 import com.maisprati.forum.exception.*;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
